66ca4d6149321413e32d0d13268f7debc6c9869c
eduhub0713-creator <eduhub0713@gmail.com>
2026-05-21 11:37:40 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,7 @@
+const CACHE_NAME = 'hub-v1';
+self.addEventListener('install', (e) => {
+    e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(['index.html', 'style.css', 'app.webmanifest'])));
+});
+self.addEventListener('fetch', (e) => {
+    e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});
